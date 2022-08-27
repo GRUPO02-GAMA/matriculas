365d904e67f75ccb4caabeab04ef11c2d5060ea4
Victor <vmtame@gmail.com>
2022-08-27 10:51:28 -0300

    wip

--- js/victor.js
+++ js/victor.js
@@ -1,3 +1,18 @@
-function editar() {
+function editar(id) {
+    tbListar[id] = JSON.stringify({
+        Matricula: $('#txtMatricula').val(),
+        Nome: $('#txtNome').val(),
+        Email: $('#txtEmail').val(),
+        Celular: $('#txtCelular').val(),
+        DtCad: $('#txtDatCad').val(),
+        HoraCad: $('#txtHoraCad').val(),
+        DtCurso: $('#txtDatCur').val(),
+        HoraCurso: $('#txtHoraCur').val(),
+        CursoDesejado: $('#txtCurDes').val(),
+        DtCursoDesejado: $('#txtDatCurDes').val(),
+        HoraCursoDesejado: $('#txtHoraCurDes').val(),
+    });
     
-}
+    localStorage.setItem("tbListar", JSON.stringify(tbListar));
+    return true;
+}  
