628032cf9e1e1d7750adce26ee345fcdb742a8d7
Victor <vmtame@gmail.com>
2022-08-27 12:21:18 -0300

    updates

--- js/adicionar.js
+++ js/adicionar.js
@@ -13,29 +13,98 @@ function GetMatricula(propriedade, valor){
 }
 
 function Adicionar() {
-    var student = GetMatricula('Matricula', $('#txtMatricula').val())
+    // var student = GetMatricula('Matricula', $('#txtMatricula').val())
 
-    if (student != null) {
-      alert('Aluno já cadastrado.')
-      return
+    // if (student != null) {
+    //   alert('Aluno já cadastrado.')
+    //   return
+    // }
+
+    var tbMatriculas = JSON.parse( localStorage.getItem('tbMatriculas') );
+
+    if( tbMatriculas === undefined ) {
+        tbMatriculas = [];
     }
 
     var aluno = JSON.stringify({
-      Matricula: $('#txtMatricula').val(),
-      Nome: $('#txtNome').val(),
-      Email: $('#txtEmail').val(),
-      Celular: $('#txtCelular').val(),
-      DtCad: $('#txtDatCad').val(),
-      HoraCad: $('#txtHoraCad').val(),
-      DtCurso: $('#txtDatCur').val(),
-      HoraCurso: $('#txtHoraCur').val(),
-      CursoDesejado: $('#txtCurDes').val(),
-      DtCursoDesejado: $('#txtDatCurDes').val(),
-      HoraCursoDesejado: $('#txtHoraCurDes').val()
-    })
+        Matricula: $('#txtMatricula').val(),
+        Nome: $('#txtNome').val(),
+        Email: $('#txtEmail').val(),
+        Celular: $('#txtCelular').val(),
+        DtCad: $('#txtDatCad').val(),
+        HoraCad: $('#txtHoraCad').val(),
+        DtCurso: $('#txtDatCur').val(),
+        HoraCurso: $('#txtHoraCur').val(),
+        CursoDesejado: $('#txtCurDes').val(),
+        DtCursoDesejado: $('#txtDatCurDes').val(),
+        HoraCursoDesejado: $('#txtHoraCurDes').val()
+    });
 
-    tbMatriculas.push(aluno)
-    localStorage.setItem('tbMatriculas', JSON.stringify(tbMatriculas))
-    alert('Aluno adicionado.')
-    return true
+    tbMatriculas.push(aluno);
+    localStorage.setItem('tbMatriculas', JSON.stringify(tbMatriculas));
+    return true;
+
+    /* */
+    // 
+    // 
+    // alert('Aluno adicionado.')
+    // return true
 }
+
+function Listar() {
+    $('#tbListar').html('')
+    $('#tbListar').html(
+      `
+        <thead>
+        <tr>
+            <th></th>
+            <th>Registro</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Celular</th>
+            <th>Data de Cadastro</th>
+            <th>Hora de Cadastro</th>
+            <th>Data para realizar</th>
+            <th>Hora pra realizar</th>
+            <th>Nome do curso</th>
+            <th>Data do curso</th>
+            <th>Hora do curso</th>
+        </tr>
+        </thead>
+        <tbody>
+        
+        </tbody>
+        `
+    )
+
+    for (var i in tbMatriculas) {
+      var cli = JSON.parse(tbMatriculas[i])
+
+      cli.DtCad = formatDate(cli.DtCad)
+      cli.DtCurso = formatDate(cli.DtCurso)
+      cli.DtCursoDesejado = formatDate(cli.DtCursoDesejado)
+      //falta fazer o tratamento de data e hora
+      $('#tbListar tbody').append(
+        `
+            <tr>
+                <td><img src="img/edit.png" alt='"+i+"' class="btnEditar"/>
+                    <img src='img/delete.png' alt='"+i+"' class='btnExcluir'/>
+                </td>
+                <td>cli.Matricula</td>
+                <td>cli.Nome</td>
+                <td>cli.Email</td>
+                <td>cli.Celular</td>
+                <td>cli.DtCad</td>
+                <td>cli.HoraCad</td>
+                <td>cli.DtCurso</td>
+                <td>cli.HoraCurso</td>
+                <td>cli.CursoDesejado</td>
+                <td>cli.DtCursoDesejado</td>
+                <td>cli.HoraCursoDesejado</td>
+            </tr>
+            `
+      )
+    }
+  }
+
+  Listar()
--- js/index.js
+++ js/index.js
@@ -38,63 +38,7 @@
     return true
   }
 
-  function Listar() {
-    $('#tbListar').html('')
-    $('#tbListar').html(
-      `
-        <thead>
-        <tr>
-            <th></th>
-            <th>Registro</th>
-            <th>Nome</th>
-            <th>E-mail</th>
-            <th>Celular</th>
-            <th>Data de Cadastro</th>
-            <th>Hora de Cadastro</th>
-            <th>Data para realizar</th>
-            <th>Hora pra realizar</th>
-            <th>Nome do curso</th>
-            <th>Data do curso</th>
-            <th>Hora do curso</th>
-        </tr>
-        </thead>
-        <tbody>
-        
-        </tbody>
-        `
-    )
-
-    for (var i in tbMatriculas) {
-      var cli = JSON.parse(tbMatriculas[i])
-
-      cli.DtCad = formatDate(cli.DtCad)
-      cli.DtCurso = formatDate(cli.DtCurso)
-      cli.DtCursoDesejado = formatDate(cli.DtCursoDesejado)
-      //falta fazer o tratamento de data e hora
-      $('#tbListar tbody').append(
-        `
-            <tr>
-                <td><img src="img/edit.png" alt='"+i+"' class="btnEditar"/>
-                    <img src='img/delete.png' alt='"+i+"' class='btnExcluir'/>
-                </td>
-                <td>cli.Matricula</td>
-                <td>cli.Nome</td>
-                <td>cli.Email</td>
-                <td>cli.Celular</td>
-                <td>cli.DtCad</td>
-                <td>cli.HoraCad</td>
-                <td>cli.DtCurso</td>
-                <td>cli.HoraCurso</td>
-                <td>cli.CursoDesejado</td>
-                <td>cli.DtCursoDesejado</td>
-                <td>cli.HoraCursoDesejado</td>
-            </tr>
-            `
-      )
-    }
-  }
-
-  Listar()
+  
 
   $('#frmCadastro').on('submit', function () {
     if (operacao == 'A') {
