ca476647eba00be7f7321d8cd5bdc68c8abfc500
Erica S. Francisco <ericasantosfrancisco@gmail.com>
2022-08-27 11:29:35 -0300

    feat: 🎸 fix names and including last line of code

--- erica.js
+++ erica.js
@@ -1,13 +1,19 @@
 function Excluir(){
-    tblListar.splice(tbMatriculas, 1);
-    localStorage.setItem("tblListar", JSON.stringify(tblListar));
+    tbMatriculas.splice(indice_selecionado, 1);
+    localStorage.setItem("tbMatriculas", JSON.stringify(tbMatriculas));
     confirm("Deseja realmente excluir");
     alert("Registro excluído.");
 }
 
 // Ação com base nos eventos do botão Excluir
 $("#tblListar").on("click", ".btnExcluir", function(){
-    tbMatriculas = parseInt($(this).attr("alt"));
+    indice_selecionado = parseInt($(this).attr("alt"));
     Excluir();
     Listar();
 });
+
+	// ultimo codigo
+	var ultimo = JSON.parse(tbMatriculas.slice(-1));
+	var ultconv = parseInt(ultimo.Matricula);
+	
+	$("#txtMatricula").val(ultconv+1);
